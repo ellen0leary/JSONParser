7db9e60298c9f92e5b6e40141dc6fc5103d95e93
ellen0leary <47063341+ellen0leary@users.noreply.github.com>
2021-03-24 22:45:20 +0000

    Updated Python Script
     now takes optional arguments

--- JSONParser.py
+++ JSONParser.py
@@ -3,8 +3,8 @@ import json #library for working with json
 
 newJson = {}
 def main(data,val =""):
-    start = ""
-    if val!="": 
+    start = "" #defines a str to add to start of key
+    if val!="":  
         start = val+'.'
     for i in data :
         if(isinstance(data[i], dict )):
@@ -12,11 +12,26 @@ def main(data,val =""):
         else:
             newJson[start+i] = data[i]
 
-f = open(sys.argv[1]) # open the file
-data = json.loads(f.read())
+file = ""
+#opens the file
+if len(sys.argv)>=2 : 
+    file =open(sys.argv[1]) 
+else : 
+    file = open("input.json")
+#loads the  data from the file and structures it as a dictionary
+data = json.loads(file.read())
+#calls method with json from input file
 main(data)
+#prints the new dictionary/json
 print(newJson)
-f.close()
-f = open("output.json", "w")
-f.write(json.dumps(newJson))
-f.close()
+#closes the input file
+file.close()
+#get output file
+if len(sys.argv)>=3:
+    file = open(sys.argv[2], "w")
+else:
+    file = open("newOutput.json", "w")
+#write to output file using the populated dictionary
+file.write(json.dumps(newJson, indent=5))
+#closes the file
+file.close()
--- output3.py
+++ output3.py
@@ -3,7 +3,6 @@
      "b": true,
      "c.d": 3,
      "c.e": "test",
-     "f": false,
-     "g.h": "word",
-     "g.i": 7
+     "f.g": 7,
+     "f.h": false
 }
